343ecac0fc604cbb2834b365557576dfb3c32025
Miki Tebeka <miki@353solutions.com>
2024-10-22 15:48:43 +0300

    simpler

--- Ch01/01_01/endian/endian.go
+++ Ch01/01_01/endian/endian.go
@@ -5,36 +5,25 @@ import (
 	"encoding/binary"
 	"fmt"
 	"math"
-	"strings"
 )
 
 func main() {
 	fmt.Println("max byte:", math.MaxUint8) // 255
 
-	var c uint16 = 256 // Ā
-	fmt.Printf("c: %d (0x%x)\n", c, c)
+	var c uint16 = 1023
+	fmt.Printf("c: %d (0x%04x)\n", c, c)
 
 	var buf bytes.Buffer
 	if err := binary.Write(&buf, binary.BigEndian, c); err != nil {
 		fmt.Println("ERROR:", err)
 		return
 	}
-	fmt.Println("big endian   :", bytesString(buf.Bytes()))
+	fmt.Printf("big endian   : %x\n", buf.Bytes())
 
 	buf.Reset()
 	if err := binary.Write(&buf, binary.LittleEndian, c); err != nil {
 		fmt.Println("ERROR:", err)
 		return
 	}
-	fmt.Println("little endian:", bytesString(buf.Bytes()))
-
-}
-
-func bytesString(bs []byte) string {
-	nums := make([]string, len(bs))
-	for i, b := range bs {
-		nums[i] = fmt.Sprintf("%02x", b)
-	}
-
-	return strings.Join(nums, " ")
+	fmt.Printf("little endian: %x\n", buf.Bytes())
 }
