d6c6877cfe7ad9c038dffe48ff58afdcfdda21a5
Miki Tebeka <miki@353solutions.com>
2024-10-23 10:59:13 +0300

    format unicode

--- Ch02/02_03/format.go
+++ Ch02/02_03/format.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"fmt"
+
+	"golang.org/x/text/feature/plural"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
 func main() {
-	c := 'A'
-	fmt.Printf("v: %v\n", c) // 65
-	fmt.Printf("c: %c\n", c) // A
-	fmt.Printf("c: %v\n", c) // 65
+	// Numbers
+	n := 1_234.56
+	fmt.Println("n:", n)
 
-	a, b := 1, "1"
-	fmt.Printf("v : a=%v, b=%v\n", a, b)   // a=1, b=1
-	fmt.Printf("#v: a=%#v, b=%#v\n", a, b) // a=1, b="1"
+	en := message.NewPrinter(message.MatchLanguage("en"))
+	en.Println("en:", n)
+	en.Printf("The cost is $%.2f\n", n)
+
+	// Plural
+	const format = "We saw %d birds\n"
+	message.Set(language.English, format,
+		plural.Selectf(1, "%d",
+			"=0", "We saw no birds :(\n",
+			plural.One, "We saw a single bird.\n",
+			plural.Other, "We saw %d birds.\n",
+		),
+	)
+
+	printer := message.NewPrinter(language.English)
+	printer.Printf(format, 0)
+	printer.Printf(format, 1)
+	printer.Printf(format, 7)
 }
